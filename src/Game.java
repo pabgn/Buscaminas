a425dea7a5adb0d0ee88510820adb2b2e7444522
Pablo García-Nieto Rodríguez <pablo@garcianieto.me>
2014-03-31 19:17:52 +0200

    Bug fixes and server settings

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,5 @@
 /**
- * Created by pablo on 27/02/14.
+ * Created by Pablo García-Nieto Rodríguez on 27/02/14.
  */
 import javax.swing.*;
 import java.awt.*;
@@ -10,12 +10,16 @@ import javax.swing.table.DefaultTableModel;
 import java.util.Random;
 import com.clwillingham.socket.io.*;
 import org.json.*;
+import javax.swing.border.*;
+import java.awt.Color;
+
 public class Game extends JPanel {
     //Creamos el Panel extendiendo a JPanel (Swing)
     private static int[][] tablero;
     private static JFrame frame;
     private static boolean alive;
     private static int numBombs;
+    private static int[][] tableroDiscovered;
     private static int numDiscovered;
     private static DefaultTableModel tableModel;
     private static DefaultTableModel pointsModel;
@@ -28,12 +32,23 @@ public class Game extends JPanel {
     static int[] remaining = new int[2];
     private static int[][] marked;
     private static int[][] markedAuthor;
+    private static int numMarked;
     private static boolean drawn;
     private static boolean reconnect=false;
+    private static JPanel panel2 = new JPanel();
+    private static JLabel textLog = new JLabel();
+    private static JTextArea tx = new JTextArea(15, 1);
+    private static JLabel perc = new JLabel();
 
     public Game() throws IOException {
         //Generamos la tabla de puntuacion
         JPanel p = new JPanel();
+        Container c = frame.getContentPane();
+        c.setLayout(new GridLayout(1,2));
+        perc.setMinimumSize(new Dimension(150, 100));
+        perc.setMaximumSize(new Dimension(150, 100));
+        perc.setAlignmentX(Component.CENTER_ALIGNMENT);
+        perc.setText("Descubierto: 0 %");
         pointsModel =  new DefaultTableModel()
         {
             @Override
@@ -53,9 +68,15 @@ public class Game extends JPanel {
         pointsTable.setRowHeight(100);
         pointsTable.getTableHeader().setReorderingAllowed(false);
         p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
+        p.add(perc);
         p.add(pointsTable);
         JButton b = new JButton("Reiniciar");
+        b.setAlignmentX(Component.CENTER_ALIGNMENT);
         p.add(b);
+        tx.setMaximumSize(new Dimension(200, 400));
+        tx.setMinimumSize(new Dimension(200, 400));
+        tx.setEditable(false);
+        p.add(tx);
         add(p);
         //Generamos la tabla del juego
         tableModel = new DefaultTableModel()
@@ -90,13 +111,13 @@ public class Game extends JPanel {
                     JTable target = (JTable)e.getSource();
                     int row = target.getSelectedRow();
                     int column = target.getSelectedColumn();
-                    if(alive && playing && marked[row][column]!=1){
+                    if(alive && playing && marked[row][column]!=1 && tablero[row][column]!=2 && tableroDiscovered[row][column]==0){
                     sendMovement(row, column);
                         playing=false;
                         pointsModel.setValueAt("Yo", 0, 0);
                         pointsModel.setValueAt("<html><b>Jugador 2<b/></html>", 1, 0);
 
-                        explorar(row, column);
+                        explorar(row, column, 0);
                     }
 
                 }
@@ -116,7 +137,6 @@ public class Game extends JPanel {
         //Interceptamos click de reinicio
         b.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-
                 iniciar();
                 reconnect=true;
                 socket.disconnect();
@@ -127,31 +147,70 @@ public class Game extends JPanel {
         });
         //A??adimos zona de scroll si fuese necesario
         JScrollPane panelConScroll = new JScrollPane(table);
-
         add(panelConScroll);
+
+
+
     }
     private static void crearUI() {
         //Creamos la pantalla inicial con su t?tulo e icono
         frame = new JFrame("Buscaminas");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
         //Configuramos el contenido
         Game panel;
         try{
+            ///
             panel = new Game();
+            JLayeredPane lpane = new JLayeredPane();
+            frame.setPreferredSize(new Dimension(710, 540));
+            frame.setLayout(new BorderLayout());
+            frame.add(lpane, BorderLayout.CENTER);
+            lpane.setBounds(0, 0, 710, 540);
+            panel.setBounds(0, 0, 710, 540);
             panel.setOpaque(true);
-            frame.setContentPane(panel);
+            /*panel2.setBackground(new Color(43, 43, 43, 170));
+            panel2.setBounds(270, 200, 300, 100);
+            panel2.setBorder(new RoundedBorder(10));
+            panel2.setOpaque(true);
+            panel2.setLayout(new GridBagLayout());
 
-            //Metodos a ser llamados para mostrar la ventana
+            panel2.add(textLog);
+            lpane.add(panel2, new Integer(1), 0);
+            */
+            lpane.add(panel, new Integer(0), 0);
             frame.pack();
             frame.setVisible(true);
+
         }catch(IOException e){
             e.printStackTrace();
         }
 
 
 
+
+
+    }
+    private static class RoundedBorder implements Border {
+
+        private int radius;
+
+        RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+        }
+
+
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x,y,width-1,height-1,radius,radius);
+        }
     }
     private static int random(){
         Random random = new Random();
@@ -164,6 +223,7 @@ public class Game extends JPanel {
         }
         return bomb;
     }
+
     private static void rellenar(int[][] tablero){
         for(int x=0;x<tablero.length;x++){
             for(int y=0;y<tablero[x].length;y++){
@@ -204,32 +264,42 @@ public class Game extends JPanel {
             tableModel.setValueAt(new ImageIcon(Game.class.getResource("icons/flag"+who+".png")), x, y);
             marked[x][y]=1;
             markedAuthor[x][y]=who;
+            numMarked++;
+            checkEnd();
 
         }
     }
-    private static void explorar(int x, int y){
-        if(x<=9 && x>=0 && y<=9 && y>=0){
+    private static void explorar(int x, int y, int t){
+        if(x<=9 && x>=0 && y<=9 && y>=0 && tableroDiscovered[x][y]==0){
         int numNear =  numNearBombs(x, y);
         if(tablero[x][y]==1){
-            //Game Over
-            recuento();
 
-            tableModel.setValueAt(new ImageIcon(Game.class.getResource("icons/mine.png")), x, y);
-            if(puntos[0]>puntos[1]){
-                JOptionPane.showMessageDialog(frame, "¡Ganaste!", "Juego", JOptionPane.INFORMATION_MESSAGE);
-            }else{
-                JOptionPane.showMessageDialog(frame, "¡Perdiste!", "Juego", JOptionPane.INFORMATION_MESSAGE);
+            tableModel.setValueAt(new ImageIcon(Game.class.getResource("icons/mine"+(t+1)+".png")), x, y);
+
+            if(t==0){
+
+
+                tx.setText("¡Descubriste una mina! -30\n"+tx.getText());
+
             }
-            alive=false;
+            puntos[t]=puntos[t]-30;
+            updatePoints();
+            tableroDiscovered[x][y]=1;
+            numDiscovered++;
         }
         else if(numNear!=0){
             if(marked[x][y]==1){
                 //Si alguien la habia marcado y claramente se descubre que no habia nada, se le restan puntos
                 puntos[markedAuthor[x][y]-1]=puntos[markedAuthor[x][y]-1]-10;
                 updatePoints();
+                if(markedAuthor[x][y]-1==0){
+                    tx.setText("Bandera equivocada -10\n"+tx.getText());
+                }
+                numMarked--;
             }
             tableModel.setValueAt(new ImageIcon(Game.class.getResource("icons/block"+numNear+".png")), x, y);
             numDiscovered++;
+            tableroDiscovered[x][y]=1;
         }
         else if(tablero[x][y]==2){
 
@@ -238,23 +308,43 @@ public class Game extends JPanel {
             if(marked[x][y]==1){
                 //Si alguien la habia marcado y claramente se descubre que no habia nada, se le restan puntos
                 puntos[markedAuthor[x][y]-1]=puntos[markedAuthor[x][y]-1]-10;
+                if(markedAuthor[x][y]-1==0){
+                    tx.setText("Bandera equivocada -10\n"+tx.getText());
+                }
+                numMarked--;
+
                 updatePoints();
             }
             tableModel.setValueAt(new ImageIcon(Game.class.getResource("icons/discovered.png")), x, y);
-            tablero[x][y]=2;
             numDiscovered++;
-            if((100-numBombs)==numDiscovered){
-                JOptionPane.showMessageDialog(frame, "¡Enhorabuena!", "Juego", JOptionPane.INFORMATION_MESSAGE);
-            }else{
-            explorar(x,y+1);
-            explorar(x,y-1);
-            explorar(x+1,y);
-            explorar(x-1,y);
+            tablero[x][y]=2;
+            tableroDiscovered[x][y]=1;
+
+            explorar(x,y+1, t);
+            explorar(x,y-1, t);
+            explorar(x+1,y, t);
+            explorar(x-1,y, t);
 
             }
-        }
+            perc.setText("Descubierto: "+numDiscovered+ "%");
         }
 
+        checkEnd();
+
+
+
+
+    }
+    public static void checkEnd(){
+        if(numDiscovered+numMarked==100){
+            recuento();
+            if(puntos[0]>puntos[1]){
+                JOptionPane.showMessageDialog(frame, "¡Ganaste!", "Juego", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(frame, "¡Perdiste!", "Juego", JOptionPane.INFORMATION_MESSAGE);
+            }
+            alive=false;
+        }
     }
     public static void updatePoints(){
         pointsModel.setValueAt(puntos[0], 0, 1);
@@ -267,11 +357,32 @@ public class Game extends JPanel {
                 if(marked[x][y]==1 && tablero[x][y]==1){
                     tableModel.setValueAt(new ImageIcon(Game.class.getResource("icons/flag"+markedAuthor[x][y]+"mine.png")), x, y);
                     puntos[markedAuthor[x][y]-1]=puntos[markedAuthor[x][y]-1]+20;
+                    if(markedAuthor[x][y]-1==0)
+                    {
+                        tx.setText("¡Bandera correcta! +20\n"+tx.getText());
+
+                    }
                     updatePoints();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
                 if(marked[x][y]==1 && tablero[x][y]!=1){
                     tableModel.setValueAt(new ImageIcon(Game.class.getResource("icons/discovered.png")), x, y);
                     puntos[markedAuthor[x][y]-1]=puntos[markedAuthor[x][y]-1]-10;
+                    if(markedAuthor[x][y]-1==0)
+                    {
+                        tx.setText("¡Bandera vacia! -10\n"+tx.getText());
+
+                    }
+                    updatePoints();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
@@ -289,6 +400,15 @@ public class Game extends JPanel {
         markedAuthor = new int[10][10];
         puntos[0]=0;
         puntos[1]=0;
+        updatePoints();
+        playing=false;
+        tableroDiscovered=new int[10][10];
+        tx.setText("");
+        //We instanciate the Timer Timer timer = new Timer();
+
+        //And then we launch the fade-out, waiting 500ms before starting
+        //Then we gradually fade out every 5ms
+
     }
     public static void showMainPanel(){
         optionPane = new JOptionPane("Conectando al servidor de juegos...", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
@@ -377,7 +497,7 @@ public class Game extends JPanel {
                 if(action.equals("doMovement")){
                     int x = a.getInt("x");
                     int y = a.getInt("y");
-                    explorar(x, y);
+                    explorar(x, y, 1);
                     pointsModel.setValueAt("<html><b>Yo<b/></html>", 0, 0);
                     pointsModel.setValueAt("Jugador 2", 1, 0);
                     playing=true;
@@ -448,7 +568,8 @@ public class Game extends JPanel {
         try {
             socket.connect();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.print("Error server");
+            //optionPane.setMessage("El servidor de juegos está inactivo o no es posible encontrarlo.");
         }
     }
 }
